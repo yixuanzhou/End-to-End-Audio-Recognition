4ee01d26fd2144fe3c4534e4fb7b9844bcd0ddd8
yixuanzhou <yixuanzhow@gmail.com>
2017-08-08 13:49:35 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,193 +1,26 @@
 # End-to-End-Audio-Recognition
-# 音频数据结果可视化页面Demo文档
-**现布在测试服(121.40.161.184)上，可直接访问http://121.40.161.184:8484/music\_voice.html与http://121.40.161.184:8484/search.html。**
-## 服务端
-###启用
-**cd /home/zhouyixuan/audio2text\_api/**
-**nohup python audio2text\_api.py > audio2text\_api.log &**
-###接口
-所有返回json的接口都必须加上跨域访问的headers，所有接口使用端口8216
-##/home/zhouyixuan/audio2text\_api/audio2text\_api.py
-###@app.route('/music/<filename>')
-返回音频
-###@app.route('/wavfiles')
-获取wav文件列表
-####返回格式为json包含:
-code: 0代表正确，非0代表错误
-err\_msg: 错误信息
-files: 文件名列表(无后缀名与路径)
-###@app.route('/m3u8\_download', methods=\['GET'])
-下载m3u8音频流并进行转码
-####返回格式为json包含:
-code: 0代表正确，非0代表错误
-err\_msg: 错误信息
-file\_name:转码后文件名
-###@app.route('/audio\_recognize', methods=\['GET'])
-音频识别并返回识别结果
-调用seg()方法
-####返回格式为json包含:
-code: 0代表正确，非0代表错误
-err\_msg: 错误信息
-file\_name: 文件名称(无后缀名与路径)
-rec\_result: 只包含1与0的列表，代表每个8秒识别为语音还是音乐，1代表语音
-###@app.route('/audio\_to\_text', methods=\['GET'])
-音频转文字并返回结果
-调用audio2text\_multiprocessing()方法
-####返回格式为json包含:
-code: 0代表正确，非0代表错误
-err\_msg: 错误信息
-text: 转文字结果列表，按先后顺序排列
-###@app.route('/search', methods=\['GET'])
-HBase关键词检索接口
-###方法
-##/home/zhouyixuan/audio2text\_api/audio\_segmenter.py
-###audio\_segment(audio\_url, seg\_points)
-###功能：
-切割音频
-####输入：
-audio\_url: 将要进行切割的wav音频文件路径
-seg\_points: 切割时间点，格式为\[\[start1, end1], \[start2, end2],...\[startn, endn]
-####输出：
-wav音频切割文件
-##/home/zhouyixuan/audio2text\_api/audio\_filter.py
-###audiofilter(flagsInd)
-####功能：
-音频滤波处理
-####输入：
-flagsInd: 一维列表，横轴为时间，纵轴为类型
-####输出：
-step: 滤波窗每一步的判断值
-result: 完成滤波的音频时间结构 
-###read\_seg\_points(audio\_step, window\_size)
-####功能：
-读取音频时间结构进行切割点的选取
-####输入：
-audio\_step: 音频时间结构
-window\_size: 滤波窗大小
-####输出：
-time\_steps: 音频类型，切割时间点
-##/home/zhouyixuan/audio2text\_api/audio\_recognition.py
-###get\_token(token\_url, api\_key, api\_secret)
-####功能：
-获取百度语音识别所需的token
-####输入：
-token\_url："https://openapi.baidu.com/oauth/2.0/token"
-api\_key："SrhYKqzl3SE1URnAEuZ0FKdT"
-api\_secret："hGqeCkaMPb0ELMqtRGc2VjWdmjo7T89d"
-####输出：
-token
-###get\_text(filename, token\_str, cu\_id, upvoice\_url, text\_result, num)
-####功能：
-返回百度语音识别转文字结果
-####输入：
-filename：待转文字的音频文件
-token\_str：由get\_token获得的token
-cu\_id：用户标识码，一般用本机mac地址
-upvoice\_url：'http://vop.baidu.com/server\_api'
-text\_result：转文字结果
-###audio2text\_multiprocessing(file\_name)
-####功能：
-多线程处理
-##train-model.py
-###aT.featureAndTrain(\["/home/yixuan/Documents/ajim/ad","/home/yixuan/Documents/ajim/sp"], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "knn", "knnMusicGenre2", False)
-####输入
-参数一，\["/home/yixuan/Documents/ajim/ad","/home/yixuan/Documents/ajim/sp"]: 训练数据路径，需要事先进行分类，文件夹名即为类型名
-参数二-五分别对应的是mtWin, mtStep, stWin, stStep
-参数六，"knn"：训练类型，其他选择有svm, extratrees, gradientboosting, randomforest
-参数七，"knnMusicGenre2"：模型保存文件名
-最后一个参数：False代表不计算长时特征，True相反。音频切割任务只能用False
-audio-classifier.py
-####功能：
-使用模型对音频进行分类切割\[flagsInd, classesAll, acc, CM] = aS.mtFileClassification("./audiofiles/c3\_0629.wav", "./knnMusicGenre2", "knn", False, './audiofiles/c3\_0629.segments')
-参数一，"./audiofiles/c3\_0629.wav"：音频文件路径
-参数二、三：模型文件路径，模型使用方法名称
-参数四： 是否给出可视化结果：若True则会给出可视化结果
-参数五：标准切割点文件路径，用来比较切割准确性。 
-/home/zhouyixuan/audio2text\_api/pythrift.py
-###scanWithKeyword(\_\_filter)
-####功能：
-对传入的参数值进行搜索
-####输入：
-\_\_filter: 搜索词
-####输出：
-HBase中所有含有此关键词的记录
+The web has been deployed on `121.40.161.184`, one can directly access on [http://121.40.161.184:8484/music_voice.html](http://121.40.161.184:8484/music_voice.html)(for audio to text translate) and [http://121.40.161.184:8484/search.html](http://121.40.161.184:8484/search.html)(for search in database through keyword).
 
-## 页面端
-###启用
-**随nginx启动**
-###Html
-##/data/ml\_project/music\_voice/music\_voice.html
-包含js/music\_voice.js，js/jquery-3.2.1.min.js
-###功能
-####line32\~52:获取wav\_file\_list
-调用@app.route('/wavfiles')获取wav文件列表并加入选择栏
-####button 开始上传并处理
-调用getRecognizedResultM3U8(m\_url)
-####button 开始处理
-调用getRecognizedResult(file\_name)
-##/data/ml\_project/music\_voice/search.html
-包含js/search.js，js/jquery-3.2.1.min.js
-###Javascript
-##/data/ml\_project/music\_voice/js/music\_voice.js
-###getRecognizedResultM3U8(m\_url)
-####功能：
-调用接口进行音频下载转码，并调用下一个音频分割的方法
-####输入：
-m\_url: m3u8地址
-####调用：
-getRecognizedResult(file\_name)
-###getRecognizedResult(file\_name)
-####功能：
-调用接口进行音频分割
-####输入：
-file\_name: wav文件名
-####调用：
-@app.route('/audio\_recognize', methods=\['GET'])
-createResultTable (rec\_result, duration)
-####输出：
-页面显示播放器
-###createResultTable (rec\_result, duration)
-####功能：
-生成音频切割结果的表格
-####输入：
-rec\_result: 识别结果
-duration: 音频时长
-####调用：
-formatSeconds(value)
-####输出：
-页面显示识别结果与button 语音转文字，点击可调用getTextResult()方法，点击识别结果中的‘语音’／‘音乐’可调用playFromTo()方法
-###getTextResult()
-####功能：
-获取所有语音转文字的结果
-####调用：
-@app.route('/audio\_to\_text', methods=\['GET'])
-####输出：
-页面显示转文字结果
-###formatSeconds(value)
-####功能：
-格式化时间
-####输入：
-value: 秒数
-####输出：
-String：将秒数格式化为HH:mm:ss的格式
-###playFromTo(stt, edt):
-####功能：
-使播放器从开始时间开始播放到结束时间自动停止
-####输入：
-stt: 开始时间
-edt: 结束时间
-##/data/ml\_project/music\_voice/js/search.js
-###search()
-####功能
-获取搜索结果
-####输入
-无
-####输出
-页面表格包含有名称内容与播放器
-###highLight(s)
-####功能
-高亮显示关键词
-####输入
-s: 关键词
-####输出
-表格内容列关键词显示红色加粗效果
+## Dependencies
+- Python 2.x
+- HBase
+- For downloading m3u8 audio stream and convert to wav files:
+    - [FFmpeg](http://ffmpeg.org/)
+- For audio classification and segmentation tasks:
+    - [PyAudioAnalysis](https://github.com/tyiannak/pyAudioAnalysis)
+- For segmenting an audio file (wav) into pieces:
+    - [Pydub](https://github.com/jiaaro/pydub)
+- For audio recognition and translate to text:
+    - [Baidu speech recognition API](http://yuyin.baidu.com/asr)
+
+## Workflow
+1. Prepare audio files for training model (`train-model.py`)
+1. Use pre-trained model to classify targeted audio segments (`audio-classifier.py`)
+1. Filter to get optimized the audio segments (`audio_filter.py`)
+1. Segment an audio file into pieces according to segment points (`audio-segmenter.py`)
+1. For each audio segment, do audio to text translation (`audio_recognition.py`)
+1. Save the result data in HBase (`pythrift.py`)
+
+## Demo
+![image](https://github.com/yixuanzhou/End-to-End-Audio-Recognition/raw/master/screenshots/audio2text.png)
+![image](https://github.com/yixuanzhou/End-to-End-Audio-Recognition/raw/master/screenshots/search.png)
--- audio2text_api.py
+++ audio2text_api.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 from audio_filter import array2list, audiofilter, read_seg_points
 from audio_segmenter import audio_segment
 from audio_recognition import audio2text_multiprocessing
@@ -16,6 +18,14 @@ sys.path.append('./hbase')
 app = Flask(__name__)
 
 
+def resp(r):
+	response = make_response(jsonify(r))
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST'
+    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+    return response
+
+
 @app.route('/music/<filename>')
 def get_file(filename):
     if os.path.exists('audiofiles/' + filename):
@@ -31,41 +41,31 @@ def rec_types():
         for t in f.readlines():
             rec_types_list.add(t)
     r = {'code': 0, 'rec_types': list(rec_types_list)}
-    response = make_response(jsonify(r))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'POST'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
-    return response
+    return resp(r)
 
 
 @app.route('/wavfiles')
 def wav_list():
     audio_dir = 'audiofiles/'
-    file_names = os.listdir(audio_dir)
-    resp = {'code': 0, 'files': list()}
-
-    for file_name in file_names:
-        if file_name.endswith('.wav'):
-            resp['files'].append(file_name)
-    response = make_response(jsonify(resp))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'POST'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
-    return response
+    filenames = os.listdir(audio_dir)
+    r = {'code': 0, 'files': list()}
+    for filename in filenames:
+        if filename.endswith('.wav'):
+            r['files'].append(filename)
+    return resp(r)
 
 
-def seg(file_name, rec_type):
-    [flagsInd, classesAll, acc, CM] = aS.mtFileClassification('audiofiles/' + file_name,
+def seg(filename, rec_type):
+    [flagsInd, classesAll, acc, CM] = aS.mtFileClassification('audiofiles/' + filename,
                                       "./" + rec_type + 'MusicGenre2', rec_type, False)
-    print(os.getcwd())
     flags = array2list(flagsInd)
     audio_step0, _ = audiofilter(flags)
-    with open(os.path.join('seg_result/', file_name + '.seg'), 'w') as f:
+    with open(os.path.join('seg_result/', filename + '.seg'), 'w') as f:
         for s in audio_step0:
             f.write(str(s) + '\n')
     seg_points = read_seg_points(audio_step0, 8)
     #seg_points = read_segment_points(audio_step0, 8)
-    audio_segment('audiofiles/' + file_name, seg_points)
+    audio_segment('audiofiles/' + filename, seg_points)
     return audio_step0, len(flagsInd)
 
 
@@ -73,38 +73,30 @@ def seg(file_name, rec_type):
 def m3u8_downloader():
     request_data = request.args
     m3u8_url = request_data.get('url')
-    file_name = urlparse.urlparse(m3u8_url).path.strip('/').replace('/', '_').split('.')[0] + '.wav'
+    filename = urlparse.urlparse(m3u8_url).path.strip('/').replace('/', '_').split('.')[0] + '.wav'
     io = StringIO()
-    io.write(u'ffmpeg -y -i {} -ab 16k -ar 16000 -ac 1 audiofiles/{}'.format(m3u8_url, file_name))
+    io.write(u'ffmpeg -y -i {} -ab 16k -ar 16000 -ac 1 audiofiles/{}'.format(m3u8_url, filename))
     cmd = io.getvalue()
     subprocess.call(cmd.split())
-    resp = {"file_name": file_name, "code": 0}
-    response = make_response(jsonify(resp))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'POST'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
-    return response
+    r = {"file_name": filename, "code": 0}
+    return resp(r)
 
 
 @app.route('/audio_recognize', methods=['GET'])
 def audio_rec():
     request_data = request.args
-    file_name = request_data.get('file')
+    filename = request_data.get('file')
     rec_type = request_data.get('rec_type')
-    result, duration = seg(file_name.encode('utf-8'), rec_type)
-    resp = {"file_name": file_name, "code": 0, "duration": duration, "rec_result": result}
-    response = make_response(jsonify(resp))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'POST'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
-    return response
+    result, duration = seg(filename.encode('utf-8'), rec_type)
+    r = {"file_name": filename, "code": 0, "duration": duration, "rec_result": result}
+    return resp(r)
 
 
 @app.route('/audio_to_text', methods=['GET'])
 def audio2text():
     request_data = request.args
-    file_name = request_data.get('file')
-    result, seg_points = audio2text_multiprocessing(file_name)
+    filename = request_data.get('file')
+    result, seg_points = audio2text_multiprocessing(filename)
     text_result = {'text': [], 'code': 0}
     for i in range(len(result)):
         r = result.get(i, 'error')
@@ -115,20 +107,15 @@ def audio2text():
     typename = {0: 'music', 1: 'speech', 2: 'bgspeech', 3: 'ad', 4: 'puremusic', 5: 'silence'}
     for s in seg_points:
         if s[2] == 1:
-            h_data.append((file_name, s[0], s[1], s[2], text_result['text'][i]))
-            fn = file_name.split('.')[0] + '_' + str(s[0]) + '_' + str(s[1]) + '.wav'
+            h_data.append((filename, s[0], s[1], s[2], text_result['text'][i]))
+            fn = filename.split('.')[0] + '_' + str(s[0]) + '_' + str(s[1]) + '.wav'
             ht.put(fn, '0', s[2], typename[s[2]], text_result['text'][i].encode('utf-8'))
             i += 1
         else:
-            h_data.append((file_name, s[0], s[1], s[2], ''))
-            fn = file_name.split('.')[0] + '_' + str(s[0]) + '_' + str(s[1]) + '.wav'
+            h_data.append((filename, s[0], s[1], s[2], ''))
+            fn = filename.split('.')[0] + '_' + str(s[0]) + '_' + str(s[1]) + '.wav'
             ht.put(fn, '0', s[2], typename[s[2]], 'None')
-
-    response = make_response(jsonify(text_result))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'POST'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
-    return response
+    return resp(text_result)
 
 
 @app.route('/search', methods=['GET'])
@@ -137,16 +124,11 @@ def scanwithkw():
     s = urlparse.unquote(request_data.get('s'))
     ht = HBaseAji(table='wahaha')
     result = ht.scanWithKeyword(s)
-    r = []
+    data = []
     for i in range(len(result)):
-        r.append({'name': result[i].row, 'value': result[i].columns['content:0'].value})
-    resp = {"code": 0, "data": r, "err_msg": 0}
-    response = make_response(jsonify(resp))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'POST'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
-    return response
-
+        data.append({'name': result[i].row, 'value': result[i].columns['content:0'].value})
+    r = {"code": 0, "data": data, "err_msg": 0}
+    return resp(r)
 
 
 if __name__ == "__main__":
